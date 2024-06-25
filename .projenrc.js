ec5a2d50732be2a4aaa58f108343e420b6bd4563
David Hessler <dhhessl@amazon.com>
2024-06-25 10:27:22 -0400

    fix: Uses projen to create autoApproval

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,10 @@
-const { awscdk } = require('projen');
+const { awscdk, github } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'David Hessler',
   authorAddress: 'dhhessl@amazon.com',
   cdkVersion: '2.20.0',
   defaultReleaseBranch: 'main',
+  gitignore: ['.DS_Store'],
   name: 'cdk-wakeful',
   repositoryUrl: 'https://github.com/aws-samples/cdk-wakeful',
   license: 'MIT-0',
@@ -11,6 +12,15 @@ const project = new awscdk.AwsCdkConstructLibrary({
     distName: 'cdk-wakeful',
     module: 'cdk_wakeful',
   },
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ['dependabot[bot]'],
+  },
+  autoMerge: true,
+  autoMergeOptions: {
+    allowedUsernames: 'dependabot[bot]',
+  },
+  github: true,
   // description: undefined,          /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                     /* Build dependencies for this module. */
   // packageName: undefined,          /* The 'name' in package.json. */
